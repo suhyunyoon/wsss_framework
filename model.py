e7f123da27e28720bc89cfc1cdb617e10e4880b2
suhyunyoon <billy555@naver.com>
2021-09-20 20:58:47 +0900

    Add other models to finetune

--- model.py
+++ model.py
@@ -1,11 +1,15 @@
 import torch
 from torch import nn
 
+import os
+import importlib
+
 from torchvision.models import resnet34, resnet50, resnet101, resnet152, resnext101_32x8d
 
 resnets = 'resnet'
 vits    = 'vits'
 vitb    = 'vitb'
+swin    = 'swin'
 # Eff
 # Swin
 # Deit
@@ -56,11 +60,18 @@ def get_model(model_name='resnet50', pretrained=False, classifier=False, class_n
         model = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50', pretrained=pretrained)
         model_type = resnets
     # External Model: IRN
-    elif model_name == 'external.irn.net.resnet50_cam':
-        model = getattr(importlib.import_module(args.cam_network), 'CAM')()
+    elif model_name == 'irn.net.resnet50_cam':
+        model_name_split = model_name.split('.')
+        # move current directory into external project
+        importlib.sys.path[0] = os.path.join('external', model_name_split[0])
+        # temporary model directory
+        model_name_ = '.'.join(model_name_split[1:])
+        model = getattr(importlib.import_module(model_name_), 'CAM')()
+        importlib.sys.path[0] = ''
+        # get pretrained path
         if pretrained:
             model.load_state_dict(torch.load(model_name + '.pth'), strict=True)
-        model_type = resnets
+        model_type = model_name_split[0] + '.' + resnets
     else:
         print('Model', model_name, 'Not Exists!')
         model = None
@@ -76,12 +87,14 @@ def get_model(model_name='resnet50', pretrained=False, classifier=False, class_n
 def get_cam_target_layer(model, model_type):
     # get target layer
     # from model.model where model is ./Classifier
-    if model_type == 'resnet': 
+    if model_type == resnets: 
         target_layer = model.layer4[-1] if hasattr(model,'layer4') else model.model.layer4[-1]
-    elif model_type == 'vits' or model_type == 'vitb':
+    elif model_type == vits or model_type == vitb:
         target_layer = model.blocks[-1].norm1 if hasattr(model, 'blocks') else model.model.blocks[-1].norm1
-    elif model_type == 'swin':
+    elif model_type == swin:
         target_layer = model.layers[-1].block[-1].norm1 if hasattr(model, 'blocks') else model.model.blocks[-1].norm1
+    elif model_type == 'irn.'+resnets:
+        target_layer = model.stage4[-1][-1] if hasattr(model, 'stage4') else model.model.stage4[-1][-1]
     else:
         target_layer = None
     return target_layer
@@ -95,24 +108,31 @@ class Classifier(nn.Module):
         # get backbone model
         self.model = model
         # remove existing linear
-        if model_type == 'resnet':
+        if model_type == resnets:
             f_num = model.fc.in_features if hasattr(model.fc, 'in_features') else 2048
             model.fc = nn.Identity()
-        elif model_type == 'vits' or model_type == 'vitb':
-            f_num = {'vits': 384, 'vitb': 768}
+        elif model_type == vits or model_type == vitb:
+            f_num = {vits: 384, vitb: 768}
             f_num = f_num[model_type]
             model.head = nn.Identity()
-        elif model_type == 'swin':
+        elif model_type == swin:
             f_num = -1
             model.head = nn.Identity()
+        # External models
+        elif model_type == 'irn.'+resnets:
+            f_num = -1
         else:
             f_num = -1
         
-        # Linear
-        self.fc = nn.Linear(f_num, num_classes)
-        # weight init
-        self.fc.weight.data.normal_(mean=0.0, std=0.01)
-        self.fc.bias.data.zero_()
+        # FC에 Linear 말고 다른 Layer를 사용 할 때
+        if model_type == 'irn.'+resnets:
+            self.fc = nn.Identity()
+        else:
+            # Linear
+            self.fc = nn.Linear(f_num, num_classes)
+            # weight init
+            self.fc.weight.data.normal_(mean=0.0, std=0.01)
+            self.fc.bias.data.zero_()
 
     def forward(self, x):
         x = self.model(x)
