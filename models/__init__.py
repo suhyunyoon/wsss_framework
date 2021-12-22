e3af5a4151653cba60f635e77bbf5cded7413d07
suhyunyoon <billy555@naver.com>
2021-12-22 23:45:12 +0900

    Add torchvision model list

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from vgg import vgg11, vgg13, vgg16, vgg19, vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
+from efficientnet import efficientnet_b0, efficientnet_b1, efficientnet_b2, efficientnet_b3, efficientnet_b4, efficientnet_b5, efficientnet_b6, efficientnet_b7
